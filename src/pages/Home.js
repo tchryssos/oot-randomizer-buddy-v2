181bf558f59feaacbe2da8e828627847ffd05508
Troy Chryssos <troychryssos@gmail.com>
2019-11-25 17:32:16 -0500

    okay I think I set up a state based context in functional world

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useCallback, useMemo } from 'react'
+import DisplayContext from 'contexts/display'
 
-const Home = () => (
-	<>
-		<div>Whoa cool website</div>
-	</>
-)
+const Home = () => {
+	const [isSimpleView, setSimpleView] = useState(true)
+	const [isRequiredOnly, setRequiredOnly] = useState(true)
+	const [isProgressionMode, setProgressionMode] = useState(false)
+	const toggleView = useCallback(
+		() => setSimpleView(!isSimpleView),
+	)
+	const toggleRequiredOnly = useCallback(
+		() => setRequiredOnly(!isRequiredOnly),
+	)
+	const toggleProgressionMode = useCallback(
+		() => setProgressionMode(!isProgressionMode),
+	)
+	const context = useMemo(() => ({
+		isSimpleView,
+		isProgressionMode,
+		isRequiredOnly,
+		toggleView,
+		toggleRequiredOnly,
+		toggleProgressionMode,
+	}), [isSimpleView, isProgressionMode, isRequiredOnly])
+
+	return (
+		<DisplayContext.Provider value={context}>
+			<div>
+				what a good site
+			</div>
+		</DisplayContext.Provider>
+	)
+}
 
 export default Home
